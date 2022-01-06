cf6e53e6fbb2bc55fd9039476570a75edae243b7
Sudip <das366966@gmail.com>
2022-01-06 16:47:44 +0530

    amazon@day2@qs5

---
+++ Amazon/numberofways@amazon@day2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        long long dp[m+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=m;i++){
+            if(i%2==0){
+                dp[i]=dp[i-1]+1;
+            }else{
+                dp[i]=dp[i-1];
+            }
+        }
+        return dp[m];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
