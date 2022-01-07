b2d85d61b37704d7152fcdb2e870002f0dafa729
Sudip <das366966@gmail.com>
2022-01-07 23:13:30 +0530

    amazon@day3@qs7

---
+++ Amazon/columnnamegivencolumnnumber@amazon@day3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string ans="";
+        while(n-->0){
+            char ch='A'+n%26;
+            ans=ch+ans;
+            n=n/26;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends
