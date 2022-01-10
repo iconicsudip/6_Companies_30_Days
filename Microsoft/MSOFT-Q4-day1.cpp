5ef280622a8aa7b41da9662c29446c8bf82d6418
Sudip <das366966@gmail.com>
2022-01-10 20:32:44 +0530

    microsoft@day1@qs1

---
+++ Microsoft/MSOFT-Q4-day1.cpp
@@ -0,0 +1,69 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        vector<int>ans;
+        int row_start = 0,row_end=r-1,column_start=0,column_end=c-1;
+        while(row_start<=row_end && column_start<=column_end){
+            for(int i=column_start;i<=column_end;i++){
+                ans.push_back(matrix[row_start][i]);
+            }
+            row_start++;
+            if(row_start > row_end || column_start > column_end){
+                break;
+            }
+            for(int i=row_start;i<=row_end;i++){
+                ans.push_back(matrix[i][column_end]);
+            }
+            column_end--;
+            if(row_start > row_end || column_start > column_end){
+                break;
+            }
+            for(int i=column_end;i>=column_start;i--){
+                ans.push_back(matrix[row_end][i]);
+            }
+            row_end--;
+            if(row_start > row_end || column_start > column_end){
+                break;
+            }
+            for(int i=row_end;i>=row_start;i--){
+                ans.push_back(matrix[i][column_start]);
+            }
+            column_start++;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
---
+++ Microsoft/MSOFT-Q5-day1.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       vector<int>ans;
+       ans.push_back(1);
+       stack<int>st;
+       st.push(0);
+       for(int i=1;i<n;i++){
+           while(!st.empty() && price[st.top()]<=price[i]){
+               st.pop();
+           }
+           if(st.empty()){
+               ans.push_back(i+1);
+           }else{
+               ans.push_back(i-st.top());
+           }
+           st.push(i);
+       }
+       return ans;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
