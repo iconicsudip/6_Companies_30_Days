cc6096fb528f67c28da18970ca4f6f2a2613499e
Sudip <das366966@gmail.com>
2022-01-10 20:33:54 +0530

    microsoft@day1@qs1

--- Microsoft/MSOFT-Q3-day1.cpp
+++ Microsoft/MSOFT-Q3-day1.cpp
@@ -5,11 +5,6 @@ using namespace std;
 void rotate (vector<vector<int> >& matrix);
 
 
- // } Driver Code Ends
-//User function template for C++
-
-/* matrix : given input matrix, you are require 
- to change it in place without using extra space */
 void rotate(vector<vector<int> >& matrix)
 {
     // Your code goes here
