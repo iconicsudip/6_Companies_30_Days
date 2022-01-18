3296d1852ca00be9919397c1dfc7dc14da817dc4
Sudip <das366966@gmail.com>
2022-01-18 23:57:16 +0530

    adobe@day3@qs9

---
+++ Adobe/ADBE-Q11-day3.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string amendSentence (string s)
+    {
+        // your code here
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(isupper(s[i])){
+                s[i]=tolower(s[i]);
+                if(i!=0){
+                    ans+=" ";
+                }
+            }
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout << ob.amendSentence (s) << endl;
+	}
+}  // } Driver Code Ends
