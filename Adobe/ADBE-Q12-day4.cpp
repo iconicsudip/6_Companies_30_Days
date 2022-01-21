c502814ac7bb754353e4b49d4f9ea6080d2fb2b6
Sudip <das366966@gmail.com>
2022-01-21 22:48:03 +0530

    adobe@day4@qs10

---
+++ Adobe/ADBE-Q12-day4.cpp
@@ -0,0 +1,60 @@
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        vector<int>ans;
+        list<int>l;
+        l.push_front(a[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(l.front()<=a[i]){
+                l.push_front(a[i]);
+            }
+        }
+        for(auto i:l){
+            ans.push_back(i);
+        }
+        l.clear();
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
